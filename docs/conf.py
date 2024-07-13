5d4dd3ae4e97e7b05cbd3aedd060c6556afd2b32
Armand Wayoff <armandwayoff@lavabit.com>
2024-07-13 22:34:57 +0200

    retest

--- docs/conf.py
+++ docs/conf.py
@@ -24,10 +24,5 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
-
-
-...
-html_theme = "sphinx_book_theme"
-...
